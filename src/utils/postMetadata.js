6e889103393527c83ed39d7f6c0a5af3630ff52c
Lạc Việt Anh <vietanhmusic@gmail.com>
2025-11-30 02:36:41 +0700

    feat: add postMetadata utility for centralized post data
    - New utility: src/utils/postMetadata.js
    - Centralize post metadata (slug, title, date, tags, excerpt)
    - Prepare for future integration with RelatedPosts component

---
+++ src/utils/postMetadata.js
@@ -0,0 +1,62 @@
+// Auto-generated post metadata from file system
+// Generated on: 2025-11-29T18:37:16.472Z
+// Do not edit manually - run: npm run generate-metadata
+
+export const postMetadata = {
+  "/oscarstudio": {
+    "createTime": 1757240953471,
+    "updateTime": 1760933084557,
+    "fileSize": 20368,
+    "lastUpdated": "2025-10-20T04:04:44.557Z",
+    "daysSinceUpdate": 40
+  },
+  "/akinet": {
+    "createTime": 1757240987106,
+    "updateTime": 1757246755926,
+    "fileSize": 10332,
+    "lastUpdated": "2025-09-07T12:05:55.926Z",
+    "daysSinceUpdate": 83
+  },
+  "/akiworkflow": {
+    "createTime": 1757241008565,
+    "updateTime": 1757246716866,
+    "fileSize": 7881,
+    "lastUpdated": "2025-09-07T12:05:16.866Z",
+    "daysSinceUpdate": 83
+  },
+  "/akiapp": {
+    "createTime": 1764396083661,
+    "updateTime": 1764397499196,
+    "fileSize": 11501,
+    "lastUpdated": "2025-11-29T06:24:59.196Z",
+    "daysSinceUpdate": 0
+  },
+  "/lamnhac": {
+    "createTime": 1760346680223,
+    "updateTime": 1760933084498,
+    "fileSize": 21412,
+    "lastUpdated": "2025-10-20T04:04:44.498Z",
+    "daysSinceUpdate": 40
+  },
+  "/tachnhac": {
+    "createTime": 1760745743105,
+    "updateTime": 1760747169704,
+    "fileSize": 17752,
+    "lastUpdated": "2025-10-18T00:26:09.704Z",
+    "daysSinceUpdate": 42
+  },
+  "/qqmusic-lyrics-search": {
+    "createTime": 1764433321159,
+    "updateTime": 1764435883092,
+    "fileSize": 27932,
+    "lastUpdated": "2025-11-29T17:04:43.092Z",
+    "daysSinceUpdate": 0
+  },
+  "/seo-system": {
+    "createTime": 1764441160046,
+    "updateTime": 1764441191648,
+    "fileSize": 17693,
+    "lastUpdated": "2025-11-29T18:33:11.648Z",
+    "daysSinceUpdate": 0
+  }
+};
